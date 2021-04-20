18813af946afdaac39437966d89d2b34ad3d1c5e
Selvakumar <preethi.selvakumar@accenture.com>
2021-04-20 18:54:59 +0530

    'build'

--- src/app/card/card.component.ts
+++ src/app/card/card.component.ts
@@ -1,27 +1,19 @@
 import { Component, OnInit, Input ,ElementRef,ViewChild} from '@angular/core';
-
-import {
-  animate,
-  AnimationBuilder,
-  AnimationFactory,
-  AnimationPlayer,
-  style,
-  trigger,state,transition
-} from "@angular/animations";
 @Component({
   selector: 'app-card',
   templateUrl: './card.component.html',
-  styleUrls: ['./card.component.css'],
-  animations: [
-  
-  ]
+  styleUrls: ['./card.component.css']
+ 
 })
 export class CardComponent implements OnInit {
  @Input() cardContent;
  @Input() isCenterCard;
  @ViewChild('canvas', { static: true }) 
 canvas: ElementRef<HTMLCanvasElement>;
+@ViewChild('canvasCenter', { static: true }) 
+canvasCenter: ElementRef<HTMLCanvasElement>;
 ctx: CanvasRenderingContext2D;
+ctxCenter: CanvasRenderingContext2D;
 requestId;
 interval;
 
@@ -44,47 +36,41 @@ constructor() {}
   }
   ngOnInit(): void {
    
-//     let canvas = document.getElementById('canvas') as
-//     HTMLCanvasElement;
-// let ctx = canvas.getContext("2d");
-// // ctx.beginPath();
-// // //ctx.rect(20, 20, 10, 10);
-// // ctx.lineTo(10, 12);
-// //     ctx.lineTo(100, 25);
-// // ctx.stroke();
-// var rectangle = new Path2D();
-// setTimeout(e =>  rectangle.rect(10, 10, 50, 50);
-
-// var circle = new Path2D();
-// circle.arc(100, 35, 25, 0, 2 * Math.PI);
-
-// ctx.stroke(rectangle);
-// ctx.fill(circle), 500);
+if(this.canvas){
+  this.ctx = this.canvas.nativeElement.getContext('2d');
+}
+if(this.canvasCenter){
+  this.ctxCenter = this.canvasCenter.nativeElement.getContext('2d');
+}
 
 
-this.ctx = this.canvas.nativeElement.getContext('2d');
 
 
 setInterval(() => {
-  // this.ctx.beginPath();
-  // this.ctx.arc(75, 75, 50, 0, Math.PI * 2, true); // Outer circle
-  // this.ctx.moveTo(110, 75);
-  // this.ctx.arc(75, 75, 35, 0, Math.PI, false);  // Mouth (clockwise)
-  // this.ctx.moveTo(65, 65);
-  // this.ctx.arc(60, 65, 5, 0, Math.PI * 2, true);  // Left eye
-  // this.ctx.moveTo(95, 65);
-  // this.ctx.arc(90, 65, 5, 0, Math.PI * 2, true);  // Right eye
-// this.ctx.strokeRect(0, 0, 60, 60);
+  
+if(this.ctx){
   this.ctx.lineWidth = 0.07;
   this.ctx.strokeStyle= '#d3d3d3';
   this.ctx.beginPath();
   this.ctx.moveTo(0, 0);
-  this.ctx.lineTo(100, 100);
+  this.ctx.lineTo(290, 150);
   this.ctx.stroke();
-  this.ctx.beginPath();
-  this.ctx.moveTo(0,100);
-  this.ctx.lineTo(100, 0);
-  this.ctx.stroke();
+}
+  
+if(this.ctxCenter){
+  this.ctxCenter.lineWidth = 0.07;
+  this.ctxCenter.strokeStyle= '#d3d3d3';
+  this.ctxCenter.beginPath();
+  this.ctxCenter.moveTo(0, 0);
+  this.ctxCenter.lineTo(290, 150);
+  this.ctxCenter.stroke();
+}
+
+
+  // this.ctx.beginPath();
+  // this.ctx.moveTo(170,15);
+  // this.ctx.lineTo(190, 130);
+  // this.ctx.stroke();
   
 }, 200);
   }
--- src/app/header/header.component.ts
+++ src/app/header/header.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component,ViewChild,ElementRef, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-header',
@@ -6,10 +6,14 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./header.component.css']
 })
 export class HeaderComponent implements OnInit {
-
+  @ViewChild('canvas', { static: true }) 
+  canvas: ElementRef<HTMLCanvasElement>;
+  ctx: CanvasRenderingContext2D;
   constructor() { }
 
   ngOnInit(): void {
+    this.ctx = this.canvas.nativeElement.getContext('2d');
+  
   }
-
 }
+
